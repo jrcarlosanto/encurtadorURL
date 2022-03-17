05d91690eddc80388c4fa72b6e81fcd99c80da5f
Carlos <carlosamtoni@gmail.com>
2022-03-17 16:48:32 -0300

    v1

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://<username>:<passoword>@url-dio.n4msy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', //adiconar a string de conexao
+}
